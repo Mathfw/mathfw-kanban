5e66c360bed074fa4517a2a478a61a4bb8c0be1a
Mathfw <mjbferreira2504@gmail.com>
2023-10-05 23:55:25 -0300

    blank

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,45 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let backlog: Array<String> = ["backlog 01", "backlog 02"]
+  let todo: Array<String> = ["todo 01", "todo 02", "todo 03", "todo 04", "todo 05"] 
+  let doing: Array<String> = ["doing 01", "doing 02", "doing 03", "doing 04"]
+  let done: Array<String> = ["done 01", "done 02", "done 03"]
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    Kanban
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div>
+    <div id="backlog">
+      {#each backlog as item }
+        <div>
+          <p>{item}</p>
+        </div>
+      {/each}
+    </div>
+    <div id="todo">
+      {#each todo as item }
+        <div>
+          <p>{item}</p>
+        </div>
+      {/each}
+    </div>
+    <div id="doing">
+      {#each doing as item }
+        <div>
+          <p>{item}</p>
+        </div>
+      {/each}
+    </div>
+    <div id="done">
+      {#each done as item }
+        <div>
+          <p>{item}</p>
+        </div> 
+      {/each}
+    </div>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
